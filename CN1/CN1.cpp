cf3d52ffc2b9c6d2fa71a0ec43ed94141c505844
K0Dango <Khangken105@gmail.com>
2025-02-22 17:10:58 +0700

    Fearture2Commit

--- CN1/CN1.cpp
+++ CN1/CN1.cpp
@@ -10,6 +10,27 @@ bool isPrime(int a) {
 	return a >= 2;
 }
 
+int nhapNam() {
+	int nam;
+	do {
+		cout << "Nhap vao nam muon kiem tra: ";
+		cin >> nam;
+		if (nam < 0) {
+			cout << "Nam phai lon hon 0!\n";
+		}
+	} while (nam < 0);
+	return nam;
+}
+
+bool KTNamNhuan(int nam) {
+	if (nam % 4 == 0)
+		return true;
+	if (nam % 100 == 0 && nam % 400 == 0)
+		return true;
+	return false;
+
+}
+
 int main() {
 	int n = 0, chon=0;
 	while (chon != 9) {
@@ -32,9 +53,13 @@ int main() {
 			else
 				cout << n << " khong phai la so nguyen to \n";
 			break;
-		case 2:
-
+		case 2: {
+			if (KTNamNhuan(nhapNam()) == true)
+				cout << "Nam do la nam nhuan!\n";
+			else
+				cout << "Nam do khong nhuan!";
 			break;
+		}
 		case 9:
 			return 0;
 			break;
